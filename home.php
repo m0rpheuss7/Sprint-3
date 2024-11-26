93b8fd24ffce71bf3a09137f31a2847b7df991fa
vittrC <169161408+vittrC@users.noreply.github.com>
2024-11-26 15:31:45 -0300

    Add files via upload

---
+++ home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title> HidratecShop </title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<audio id="background-music" loop autoplay>
+        <source src="audio.mp3" type="audio/mpeg">
+    </audio>    
+
+    <div id="bg">
+  <canvas></canvas>
+  <canvas></canvas>
+  <canvas></canvas>
+</div>
+
+<header class="header">
+    <div class="logo">
+        <h1>HidratecShop</h1>
+    </div>
+    <nav class="navigation">
+        <ul>
+            <li><a href=".php">Início</a></li>
+            <li><a href=".php">Produtos</a></li>
+            <li><a href=".php">Contrato de serviços</a></li>
+            <li><a href=".php"><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+  <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+</svg></li></a>
+        </ul>
+    </nav>
+</header>
+
+</body>
+</html>
