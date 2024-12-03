c15ce31909916dffd52b7ba884be04a03880b076
Saravtb <162340362+Saravtb@users.noreply.github.com>
2024-12-03 09:06:45 -0300

    mudei nada

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "sistema_cadastro";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
